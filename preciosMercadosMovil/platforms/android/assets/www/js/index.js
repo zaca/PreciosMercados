318d0ed02b0a951880c61df1657dc14faeac379f
zaca <zmonzon@gmail.com>
2017-05-10 15:45:46 -0300

    movileApp first service invoke

---
+++ preciosMercadosMovil/platforms/android/assets/www/js/index.js
@@ -0,0 +1,135 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+	// Application Constructor
+	initialize : function() {
+		document.addEventListener('deviceready', this.onDeviceReady.bind(this),
+				false);
+	},
+
+	// deviceready Event Handler
+	//
+	// Bind any cordova events here. Common events are:
+	// 'pause', 'resume', etc.
+	onDeviceReady : function() {
+		// this.receivedEvent('deviceready');
+		callButton = document.getElementById("callButton");
+		callButton.addEventListener('click', this.callButtonClick, false);
+	},
+
+	// Update DOM on a Received Event
+	receivedEvent : function(id) {
+		// var parentElement = document.getElementById(id);
+		// var listeningElement = parentElement.querySelector('.listening');
+		// var receivedElement = parentElement.querySelector('.received');
+
+		// listeningElement.setAttribute('style', 'display:none;');
+		// receivedElement.setAttribute('style', 'display:block;');
+
+		console.log('Received Event: ' + id);
+	},
+
+	callButtonClick : function() {
+		container = document.getElementById("serviceResult");
+		var response = callRestService(container);
+	}
+
+};
+
+app.initialize();
+
+function callRestService(container) {
+	var response = "";
+	var url = "http://34.204.253.238:8080/concentrador/rest/quotation/byCode/123";
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200) {
+				var lista = JSON.parse(this.responseText);
+				visualization(lista, container);
+			} else {
+				container.innerHTML = xhr.statusText;
+			}
+		}
+	};
+	try {
+		xhr.open('GET', url, true);
+		xhr.send();
+	} catch (err) {
+		container.innerHTML = err.message;
+	}
+};
+
+function visualization(arr, element) {
+	var table = document.createElement("TABLE");
+    table.setAttribute("id", "productsTable");
+    table.setAttribute('class', 'contentTable');
+    element.appendChild(table);
+    
+    /*Cabecera*/
+    line = document.createElement("TR");
+    line.setAttribute('class', 'contentLineHeader');
+	table.appendChild(line);
+
+	td1 = document.createElement("TD");
+	cellContent1 = document.createTextNode("Descripcion");
+	td1.appendChild(cellContent1);
+	line.appendChild(td1);
+	
+	td2 = document.createElement("TD");
+	cellContent2 = document.createTextNode("Precio máximo");
+	td2.appendChild(cellContent2);
+	line.appendChild(td2);
+	
+	td3 = document.createElement("TD");
+	cellContent3 = document.createTextNode("Precio mínimo");
+	td3.appendChild(cellContent3);
+	line.appendChild(td3);
+	
+	td4 = document.createElement("TD");
+	cellContent4 = document.createTextNode("Zona");
+	td4.appendChild(cellContent4);
+	line.appendChild(td4);
+    
+    /*Contenido*/
+    for (i = 0; i < arr.length && i < 10; i++) {
+    	line = document.createElement("TR");
+    	table.appendChild(line);
+
+    	td1 = document.createElement("TD");
+    	cellContent1 = document.createTextNode(arr[i].description);
+    	td1.appendChild(cellContent1);
+    	line.appendChild(td1);
+    	
+    	td2 = document.createElement("TD");
+    	cellContent2 = document.createTextNode(arr[i].maxValue);
+    	td2.appendChild(cellContent2);
+    	line.appendChild(td2);
+    	
+    	td3 = document.createElement("TD");
+    	cellContent3 = document.createTextNode(arr[i].minValue);
+    	td3.appendChild(cellContent3);
+    	line.appendChild(td3);
+    	
+    	td4 = document.createElement("TD");
+    	cellContent4 = document.createTextNode(arr[i].source);
+    	td4.appendChild(cellContent4);
+    	line.appendChild(td4);
+    }
+};
---
+++ preciosMercadosMovil/platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,41 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device-orientation/www/CompassError.js",
+        "id": "cordova-plugin-device-orientation.CompassError",
+        "pluginId": "cordova-plugin-device-orientation",
+        "clobbers": [
+            "CompassError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-orientation/www/CompassHeading.js",
+        "id": "cordova-plugin-device-orientation.CompassHeading",
+        "pluginId": "cordova-plugin-device-orientation",
+        "clobbers": [
+            "CompassHeading"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-orientation/www/compass.js",
+        "id": "cordova-plugin-device-orientation.compass",
+        "pluginId": "cordova-plugin-device-orientation",
+        "clobbers": [
+            "navigator.compass"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-orientation/src/browser/CompassProxy.js",
+        "id": "cordova-plugin-device-orientation.CompassProxy",
+        "pluginId": "cordova-plugin-device-orientation",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-device-orientation": "1.0.7",
+    "cordova-plugin-whitelist": "1.3.2"
+}
+// BOTTOM OF METADATA
+});
